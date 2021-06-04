319f93efc85b74b2aa7b82718564c11f6f17720f
JoseMauricioBazagoitiaAyllon <84250913+JoseMauricioBazagoitiaAyllon@users.noreply.github.com>
2021-06-03 23:49:01 -0400

    Add files via upload
    Cambio 3/6/2021

--- src/Autenticacion/Autenticacion.js
+++ src/Autenticacion/Autenticacion.js
@@ -3,6 +3,98 @@ const mysqlConnection  = require('../BaseDeDatos');
 const app = express();
 const jwt = require("jsonwebtoken");
 const router = express.Router();
+router.post('/login', async(req, res) => {
+    const {User_Name,User_Email,User_password} = req.body;
+    console.log(User_Name,User_Email,User_password);
+    if(User_Name  && User_Email  && User_password ){
+        mysqlConnection.query('Select * from administradores WHERE User_Name = ? && User_Email = ? && User_password = ?',[User_Name,User_Email,User_password],async (err, rows)=>{
+            if(rows.length == 0){
+                res.send('Acceso denegado');
+            }else{
+                //res.send('Acceso Dado')
+                console.log(rows);
+                console.log(User_Name);
+                const user = {
+                    User_Name: User_Name,
+                    User_Email: User_Email,
+                    User_password: User_password
+                }
+                jwt.sign({user}, 'secretkey',{ expiresIn: '1h' },(err, token) => {
+                    res.json({
+                        token 
+                    });
+                });
+
+            }
+        })
+    }else{
+        res.send('Por Favor revise los datos')
+    }
+});
+module.exports = router;
+
+/*
+class login{
+     login = async(req,res)=>{
+        const {nombre,email} = req.body;
+        console.log(nombre,email);
+        if(!(nombre && email)){
+            return res.status(400).json({message : 'User & Email are requied'});
+        }
+        return res.json({message : 'Todo Okey'});
+    }
+    login;
+}
+module.exports = {login};
+
+
+/*
+router.post('/login', (req, res) => {
+    const {nombre,email} = req.body;
+    console.log(nombre,email);
+    const user = {
+        nombre : "Mauricio",
+        email: "Bazagoitia.ayllon.josemauricio@gmail.com"
+    }
+    console.log({nombre, email});
+    console.log(user.nombre);
+    if(nombre == user.nombre && email == user.email){
+        jwt.sign({user}, 'secretkey',{ expiresIn: '1h' },(err, token) => {
+            res.json({
+                token
+            });
+        });
+    }else{
+        res.json({status: 'User invalido'});
+    }    
+});
+router.post("/api/posts", verifyToken, (req , res) => {
+
+    jwt.verify(req.token, 'secretkey', (error, authData) => {
+        if(error){
+            res.json({
+                mensaje: "Acceso denegado"
+            });
+        }else{
+            res.json({
+                    mensaje: "Post fue creado",
+                    authData
+                });
+        }
+    });
+});
+function verifyToken(req, res, next){
+    const bearerHeader =  req.headers['authorization'];
+  
+    if(typeof bearerHeader !== 'undefined'){
+         const bearerToken = bearerHeader;
+         req.token  = bearerToken;
+         next();
+    }else{
+        res.sendStatus(403);
+    }
+  }
+module.exports = router;
 /*
 function login(req, res){
     console.log('insertar');
@@ -61,49 +153,3 @@ function login(req, res){
     }
     //res.json();
 }*/
-router.post('/login', (req, res) => {
-    const {nombre,email} = req.body;
-    console.log(nombre,email);
-    const user = {
-        nombre : "Mauricio",
-        email: "Bazagoitia.ayllon.josemauricio@gmail.com"
-    }
-    console.log({nombre, email});
-    console.log(user.nombre);
-    if(nombre == user.nombre && email == user.email){
-        jwt.sign({user}, 'secretkey',{ expiresIn: '1h' },(err, token) => {
-            res.json({
-                token
-            });
-        });
-    }else{
-        res.json({status: 'User invalido'});
-    }    
-});
-router.post("/api/posts", verifyToken, (req , res) => {
-
-    jwt.verify(req.token, 'secretkey', (error, authData) => {
-        if(error){
-            res.json({
-                mensaje: "Acceso denegado"
-            });
-        }else{
-            res.json({
-                    mensaje: "Post fue creado",
-                    authData
-                });
-        }
-    });
-});
-function verifyToken(req, res, next){
-    const bearerHeader =  req.headers['authorization'];
-  
-    if(typeof bearerHeader !== 'undefined'){
-         const bearerToken = bearerHeader;
-         req.token  = bearerToken;
-         next();
-    }else{
-        res.sendStatus(403);
-    }
-  }
-module.exports = router;
